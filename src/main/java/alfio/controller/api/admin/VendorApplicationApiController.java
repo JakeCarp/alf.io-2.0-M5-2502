c7fbf4139c25280602deb5483912fac64da76fdf
JakeCarp <jakecarpenter407@gmail.com>
2025-07-28 12:38:45 -0600

    fix route

--- src/main/java/alfio/controller/api/admin/VendorApplicationApiController.java
+++ src/main/java/alfio/controller/api/admin/VendorApplicationApiController.java
@@ -36,7 +36,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/admin/api/v2/event/{eventName}/vendor-application")
+@RequestMapping("/admin/api/event/{eventName}/vendor-application")
 public class VendorApplicationApiController {
     private final VendorApplicationManager vendorApplicationManager;
 
